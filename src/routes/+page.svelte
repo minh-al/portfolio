418c0da813ac312b5a2abbf12fc1efa1bbb87fe3
immortaldogg <laminh711@gmail.com>
2024-03-01 23:51:26 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<p class="text-green-400">Aha</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
